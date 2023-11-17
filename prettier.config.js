a28788b4916ad28a9547ca5b7fc14d6e9d532291
Matthias <matthias0@skiff.com>
2023-11-17 12:33:25 +0100

    Install and setup prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "avoid",
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["clsx"],
+}
