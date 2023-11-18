faaccdcef0b4c35a546124df2b3b0d8c1300996b
Matthias <matthias0@skiff.com>
2023-11-18 14:29:47 +0100

    Change starter code

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+export default function HeaderComponent() {
+  return (
+    <header className="p-6">
+      <nav>
+        <Link href="/" aria-label="Home">
+          Home
+        </Link>
+        <span>, </span>
+        <Link href="/" aria-label="About">
+          About
+        </Link>
+      </nav>
+    </header>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,25 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
+    fontFamily: {
+      sans: ["var(--font-inter)"],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        "-sm": { max: "639px" },
+        "-md": { max: "767px" },
+        "-lg": { max: "1023px" },
+        "-xl": { max: "1279px" },
+        "-2xl": { max: "1535px" },
       },
     },
   },
