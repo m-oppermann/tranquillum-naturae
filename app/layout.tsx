72714862a818592b4685c4d9d3e80a86c4c3c12e
Matthias <matthias0@skiff.com>
2023-11-19 18:26:24 +0100

    Setup document grid

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,12 +18,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <html lang="en">
       <body
         className={clsx(
-          "bg-sand-1 text-sand-8 font-sans font-medium antialiased",
+          "bg-sand-1 text-sand-8 grid h-screen grid-cols-12 grid-rows-6 gap-4 px-8 py-6 font-sans font-medium antialiased",
           NeueMontreal.variable,
         )}
       >
-        <Header />
-        <main>{children}</main>
+        <Header className="col-span-12 row-span-1 row-start-1" />
+        <main className="col-span-12 row-span-5 row-start-2">{children}</main>
       </body>
     </html>
   )
